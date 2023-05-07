eefeb99be224b05ef329ef94dba5479e7e4fc070
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-05-07 00:40:53 -0600

    Added Hooks

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+
+export const useFetchGifs = ( category ) => {
+
+    const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getImages = async () => {
+        const newImage = await getGifs( category );
+        setImages(newImage);
+        setIsLoading(false);
+    }
+
+    useEffect( () => {
+        getImages();
+    }, []);
+   
+    
+    return {
+        images: images,
+        isLoading
+    }
+}

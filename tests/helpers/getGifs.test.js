8ef37355b67bd47dd9855a54cc7ff67baefc8ea1
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-05-07 10:06:37 -0600

    Added test Helpers

---
+++ tests/helpers/getGifs.test.js
@@ -0,0 +1,19 @@
+import { getGifs } from "../../src/helpers/getGifs"
+
+
+describe('Probando helper getGifs', () => {
+   test('debe de retornar un arreglo de gifs', async() => {
+     
+    const gifs = await getGifs('Spiderman');
+
+    expect( gifs.length ).toBeGreaterThan( 0 );
+
+    expect( gifs[0] ).toEqual({
+        id: expect.any( String ),
+        title: expect.any( String ),
+        url: expect.any( String ),
+    });
+
+   });
+   
+})
